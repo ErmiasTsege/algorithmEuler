45a1be0c61f65baacedbe379e5e149d1aa77eea1
Ermias Tsege <ermias.tsege@one.verizon.com>
2022-08-16 15:28:46 -0500

    Recurssion

--- recursion/recursion1.js
+++ recursion/recursion1.js
@@ -1,4 +1,16 @@
 // Write a function, sumNums, that takes a number, num, as an argument and returns the sum of all the numbers between 1 and num.
 // You can assume that num will be greater than 1. Use recursion.
+ sum=0
 
-sumNums(3); // => 6 (3 + 2 + 1)
+function sumNums(num){
+     sum=sum+num;
+    // for (i=num;i>1;i--){
+    //   sum+=i;
+
+    // }
+  if(num>0)  {sumNums(num-1)}
+
+ return sum;
+}
+
+console.log(sumNums(7)); // => 6 (3 + 2 + 1)
--- recursion/recursion2.js
+++ recursion/recursion2.js
@@ -1,4 +1,16 @@
 // Write a function, countVowels, that accepts a string as an argument and returns the number of vowels in that string.
 // Use recursion.
 
-countVowels('Four score and seven years'); // => 9
+count=0
+function countVowels(str)
+
+ {       
+     if( str[str.length-1].includes('a')||str[str.length-1].includes('e')||str[str.length-1].includes('i')||str[str.length-1].includes('o')||str[str.length-1].includes('u')==true)
+      { count++  }
+      i=str.length-1;
+      str=str.slice(0, str.length - 1);
+
+   if(i>0){countVowels(str)}
+    return count
+  }
+console.log(countVowels("Four score and seven years")); // => 9
